42c1bb2e5677f0f81bf6aadd1a6c4a326e4ac965
uraltunaboyu <ural2000@gmail.com>
2020-07-02 18:37:50 -0700

    Added graphing w/dummy values

--- frontend/chart.js
+++ frontend/chart.js
@@ -1,3 +1,42 @@
 import React from 'react'
 import { Line } from 'react-chartjs-2'
 
+class Graph extends React.Component {
+    initialState = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: this.props.title,
+          fill: false,
+          lineTension: 0.1,
+          backgroundColor: "rgba(75, 192, 192, 0.4)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderCapStyle: "butt",
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: "miter",
+          pointBorderColor: "rgba(75, 192, 192, 1)",
+          pointBackgroundColor: "#fff",
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: "rgba(75, 192, 192, 1)",
+          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: this.props.values
+        }
+      ]
+    };
+  
+    render() {
+      return (
+        <div>
+          <h2>{this.props.graphTitle}</h2>
+          <Line data={this.initialState} />
+        </div>
+      );
+    }
+  }
+  
+  export default Graph;
---
+++ frontend/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import app from './app'
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
