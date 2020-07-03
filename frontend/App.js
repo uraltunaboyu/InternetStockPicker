144f0f6153f4152b21b98a6580f8615e667c55ef
uraltunaboyu <ural2000@gmail.com>
2020-07-02 19:27:44 -0700

    Added React packages

--- frontend/App.js
+++ frontend/App.js
@@ -2,18 +2,17 @@ import React from 'react';
 import 'style.css';
 import nasdaqVals from '../data/nasdaq.json'
 import Chart from './chart.js'
-export default function app() {
+export default function App() {
     return(
         <div className="app">
-            <h1 id="title">
+            <h1 id="title">Test</h1>
                 <ul>
                     {nasdaqVals.map(entry => {
                         <li key = {entry.Symbol}>
-                            <Chart title={entry["Security Name"]} values={Array.from({length: 40}, () => Math.floor(Math.random() * 40))}/>
+                            <Chart title={entry["Security Name"]} values={Array.from({length: 21}, () => Math.floor(Math.random() * 200 + 100))}/>
                         </li>
                     })}
                 </ul>
-            </h1>
         </div>
     )
 }
--- frontend/index.js
+++ frontend/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import app from './app'
+import App from './App.js'
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root")
 ReactDOM.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
-)
+    </React.StrictMode>,
+    rootElement
+);
