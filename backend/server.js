2fb45335c686c53a8a8092dde01c39c3d3b86149
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-07-02 22:02:25 -0400

    /nasdaq servers the json.

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve('../frontend/index.html'));
 });
 
+app.get('/nasdaq', (req, res) => {
+
+    fs.readFile(path.resolve('../data/nasdaq.json'), (err, json) => {
+        let obj = JSON.parse(json);
+        res.json(obj);
+    });
+
+});
 
 const port = 8080;
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`))
