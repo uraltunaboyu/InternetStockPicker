1de77c7f2fb246a4882091bf84f0b727d1855ebd
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-07-08 12:45:29 -0400

    Removed divider from dropdown.

--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -25,7 +25,6 @@ class Navigation extends React.Component {
                                         <NavDropdown.Item href="#action/3.1">{moment().startOf('isoWeek').format('MMMM Do YYYY')}</NavDropdown.Item>
                                         <NavDropdown.Item href="#action/3.2">{moment().subtract(1, 'weeks').startOf('isoWeek').format('MMMM Do YYYY')}</NavDropdown.Item>
                                         <NavDropdown.Item href="#action/3.3">{moment().subtract(2, 'weeks').startOf('isoWeek').format('MMMM Do YYYY')}</NavDropdown.Item>
-                                        <NavDropdown.Divider />
                                     </NavDropdown>
                                     <Nav.Link href="#home">Tables</Nav.Link>
                                     <Nav.Link href="#footer">About us</Nav.Link>
