6c055c0c85e22b7bec49d58804002cde490eadbc
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-07-09 07:27:50 -0400

    new comps in dummy

--- frontend/src/dummy.js
+++ frontend/src/dummy.js
@@ -9,6 +9,54 @@ export default [
       ticker: "AAN",
       name: "Aaron's, Inc. Common Stock",
       values: [300, 350, 340, 330, 300, 320, 330, 325]
+    },
+
+    {
+      ticker: "AAPL",
+      name: "Apple, Inc. Common Stock",
+      values: [400, 150, 75, 32, 234, 85, 75, 983]
+    },
+
+    {
+      ticker: "GOOG",
+      name: "Alphabet Inc. Common Stock",
+      values: [125, 431, 125, 748, 999, 15, 25, 53]
+    },
+
+    {
+      ticker: "Test",
+      name: "Test Inc. Common Stock",
+      values: [54, 32, 65, 875, 65, 25, 321, 125]
+    },
+
+    {
+      ticker: "MSFT",
+      name: "Microsoft Inc. Common Stock",
+      values: [634, 653, 215, 136, 542, 678, 333, 654]
+    },
+
+    {
+      ticker: "AMD",
+      name: "AMD Inc. Common Stock",
+      values: [456, 452, 765, 456, 321, 567, 432, 54]
+    },
+
+    {
+      ticker: "HAL",
+      name: "Halliburton Inc. Common Stock",
+      values: [65, 468, 665, 456, 431, 356, 153, 542]
+    },
+
+    {
+      ticker: "PGM",
+      name: "PureGoldMine Inc. Common Stock",
+      values: [135, 422, 564, 342, 655, 542, 321, 644]
+    },
+
+    {
+      ticker: "RY",
+      name: "RBC Inc. Common Stock",
+      values: [125, 653, 421, 463, 653, 235, 654, 325]
     }
   ];
   
