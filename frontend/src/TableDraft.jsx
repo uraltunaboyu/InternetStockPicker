3ff0ac136abc6dc6d28ce1805af566f4d8d47104
uraltunaboyu <ural2000@gmail.com>
2020-07-08 21:21:33 -0700

    Almost fully implemented table

--- frontend/src/TableDraft.jsx
+++ frontend/src/TableDraft.jsx
@@ -1,76 +1,62 @@
-import { useTable } from 'react-table'
-import React from 'react'
-import { Table } from 'react-bootstrap'
+import React from "react";
+import BootstrapTable from "react-bootstrap-table-next";
+import filterFactory, { textFilter, selectFilter } from 'react-bootstrap-table2-filter'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import values from './s&p500.json'
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 
-export default function DataTable() {
-    const data = React.useMemo(
-        () => [
-            {}
-        ]
-    )
-
-    const columns = React.useMemo( 
-        () => [
-            {
-                Header: 'Securities Name',
-                accessor: 'name'
-            },
-            {
-                Header: 'Ticker',
-                accessor: 'ticker'
-            },
-            {
-                Header: 'Rank',
-                accessor: 'rank'
-            },
-            {
-                Header: 'Last Rank',
-                accessor: 'lastrank'
-            }
-        ]
-    )
-
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow
-    } = useTable({ columns, data })
-
-    return (
-        <Table {...getTableProps()}>
-          <thead>
-            {headerGroups.map(headerGroup => (
-              <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map(column => (
-                  <th
-                    {...column.getHeaderProps()}
-                  >
-                    {column.render('Header')}
-                  </th>
-                ))}
-              </tr>
-            ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-            {rows.map(row => {
-              prepareRow(row)
-              return (
-                <tr {...row.getRowProps()}>
-                  {row.cells.map(cell => {
-                    return (
-                      <td
-                        {...cell.getCellProps()}
-                      >
-                        {cell.render('Cell')}
-                      </td>
-                    )
-                  })}
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
-      )
+const sectorOptions = {
+    0: "Industrials",
+    1: "Health Care",
+    2: "Information Technology",
+    3: "Communication Services",
+    4: "Communcation Discretionary",
+    5: "Consumer Staples",
+    6: "Materials",
+    7: "Real Estate",
+    8: "Utilities",
+    9: "Financials",
+    10: "Energy"
 }
+
+const columns = [
+  {
+    dataField: "Name",
+    text: "Company Name",
+    sort: true,
+    filter: textFilter(),
+  },
+  {
+    dataField: "Symbol",
+    text: "Ticker",
+    sort: true,
+    filter: textFilter(),
+  },
+  {
+    dataField: "Sector",
+    text: "Company Sector",
+    filter: selectFilter({
+        options: sectorOptions
+    })
+  },
+  {
+    dataField: "rank",
+    text: "Rank",
+    sort: true,
+  },
+  {
+    dataField: "lastrank",
+    text: "Last Rank",
+    sort: true,
+  },
+];
+
+export default () => (
+  <BootstrapTable
+    keyField="rank"
+    data={values}
+    columns={columns}
+    filter={filterFactory()}
+    pagination = { paginationFactory() }
+  />
+);
