2d9f124cffed9af70c356b41538cc8e89deea444
uraltunaboyu <ural2000@gmail.com>
2020-07-23 15:08:42 -0700

    Made MainGraph a stateful component

--- frontend/src/MainGraph.jsx
+++ frontend/src/MainGraph.jsx
@@ -3,14 +3,23 @@ import dummyVals from "./dummy";
 import Graph from "./Graph.jsx";
 import { Col } from "react-bootstrap";
 
-export default function MainGraph(props) {
-  const graphValues = (dummyVals.length > 5) ? (dummyVals.splice(0, 5)) : dummyVals;
+class MainGraph extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      graphValues : (dummyVals.length > 5) ? (dummyVals.splice(0, 5)) : dummyVals,
+    }
+  }
 
-  return (
-    <>
-      <Col>
-        <Graph data={dummyVals} />
-      </Col>
-    </>
-  );
+  render() {
+    return (
+      <>
+        <Col>
+          <Graph data={this.state.graphValues} />
+        </Col>
+      </>
+    );
+  }
 }
+
+export default MainGraph;
