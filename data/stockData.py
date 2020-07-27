413e5b1a8d4df37850687141b88857ba040e6dca
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-07-27 16:17:31 -0400

    working close for top 10

--- data/stockData.py
+++ data/stockData.py
@@ -1,25 +1,33 @@
 import json
 from alpha_vantage.timeseries import TimeSeries
-from pprint import pprint
+import time
 
 
 
 with open('./companyMentioned.json', 'r') as s:
     comps = json.load(s)
 
+def extractMentions(json):
+    try:
+        return int(json['Mentions'])
+    except KeyError:
+        return 0
+
+# This function returns the value of Mentions.
+
+comps.sort(key=extractMentions, reverse=True)
+
+
 ts = TimeSeries(key='0RWDK629LMHCGPMI', output_format='pandas')
 
-
-""" for company in comps:
-    data, meta_data = ts.get_intraday(symbol=company["ACT Symbol"])
-    for i, date in enumerate(data):
-        if i == 1:
-            company["LastClose"] = data[i]['close'] """
-
-
-data, meta_data = ts.get_daily(symbol='MSFT', outputsize='full')
-print(data.iloc[0]['4. close'])
+i = 0
+for company in comps:
+    if i < 10:
+        data, meta_data = ts.get_daily(symbol=company["ACT Symbol"], outputsize='full')
+        company["Close"] = data.iloc[0]['4. close']
+        i += 1
+        time.sleep(12)
 
 
-""" with open('companyMentioned_Stock.json', 'w') as outfile:
-    json.dump(comps, outfile)   """
+with open('companyMentioned_Stock.json', 'w') as outfile:
+    json.dump(comps, outfile)  
