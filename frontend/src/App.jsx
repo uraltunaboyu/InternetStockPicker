7a6967c7c906eb492585d9c283b68f8f043ac6b1
Kemal Kongar <47402993+kemalkongar@users.noreply.github.com>
2020-08-10 22:44:32 -0400

    - redun

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import MainGraph from "./MainGraph";
 import Navbar from "./Navigation";
-import TableDraft from "./TableDraft";
 import LineExample from './ChartsJSTest';
 import "bootswatch/dist/darkly/bootstrap.min.css";
 import { Container, Row } from 'react-bootstrap'
