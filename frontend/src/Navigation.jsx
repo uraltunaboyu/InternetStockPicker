fa85c853aba3532de1e6d863db7098b27d48a983
uraltunaboyu <ural2000@gmail.com>
2020-09-08 20:36:32 -0700

    Added rudimentary datepicker

--- frontend/src/Navigation.jsx
+++ frontend/src/Navigation.jsx
@@ -4,7 +4,7 @@ import {
   Nav,
   NavDropdown,
 } from "react-bootstrap";
-import DatePicker from 'react-datepicker';
+import DatePicker from './Datepicker'
 var moment = require("moment");
 moment().format();
 
@@ -17,7 +17,7 @@ export default function Navigation(props) {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
           <NavDropdown title="Select a Day" id="basic-nav-dropdown">
-            <DatePicker selected={startDate} onChange = {date => setStartDate(date)}/>
+            <DatePicker/>
           </NavDropdown>
           <Nav.Link href="#graph">Graph</Nav.Link>
           <Nav.Link href="#table">Table</Nav.Link>
